bd049859050cf95b1c3473e90d8604daaf21ea3c
BelugaChan <tuchnyy@list.ru>
2025-04-08 19:51:27 +0300

    Added an interface for hack service

---
+++ MethodologyMain.Application/Interface/IHackathonService.cs
@@ -0,0 +1,16 @@
+﻿using MethodologyMain.Application.DTO;
+using MethodologyMain.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodologyMain.Application.Interface
+{
+    public interface IHackathonService
+    {
+        Task<List<Hackathon>> GetHackathonsByFlexibleSearchAsync(HackathonFilterDto filterDto);
+    }
+}
