2497ea0d87a250ab41024f5d8316217e9a941132
BelugaChan <tuchnyy@list.ru>
2025-04-08 19:58:31 +0300

    Added new hack interface

---
+++ MethodologyMain.Persistence/Interfaces/IHackathonRepository.cs
@@ -0,0 +1,16 @@
+﻿using MethodologyMain.Logic.Entities;
+using System.Linq.Expressions;
+
+namespace MethodologyMain.Persistence.Interfaces
+{
+    public interface IHackathonRepository
+    {
+        Task<HackathonEntity?> GetByIdAsync(Guid id);
+
+        Task<List<HackathonEntity>> GetAllCurrentHackathonsAsync(int page = 1, int pageSize = 10);
+
+        Task<List<HackathonEntity>> GetAllHackathonsAsync(int page = 1, int pageSize = 10);
+
+        Task<List<HackathonEntity>> GetHackathonsByFlexibleSearchAsync(int page, int pageSize, Expression<Func<HackathonEntity, bool>> filter = null);
+    }
+}
