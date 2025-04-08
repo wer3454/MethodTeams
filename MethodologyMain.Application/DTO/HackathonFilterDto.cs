e3d65044fd128b2c2dfa7f55a535e6d3cd5aa57f
BelugaChan <tuchnyy@list.ru>
2025-04-08 19:50:37 +0300

    Added new Dto for hack filtering

---
+++ MethodologyMain.Application/DTO/HackathonFilterDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodologyMain.Application.DTO
+{
+    public class HackathonFilterDto
+    {
+        public string? Name { get; set; }
+        public Guid? OrganizationId { get; set; }
+        public decimal? Prize { get; set; }
+
+        public int? MinTeamSize { get; set; }
+
+        public int? MaxTeamSize { get; set; }
+
+        public DateTime? StartDateFrom { get; set; }
+
+        public DateTime? StartDateTo { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
