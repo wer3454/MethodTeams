a5c708c362293815890d5c63445e1dc7e3764521
BelugaChan <tuchnyy@list.ru>
2025-04-10 19:43:21 +0300

    Added implementation of rabbit and log publish interfaces
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ MethodologyMain.Infrastructure/Services/LogQueueService.cs
@@ -0,0 +1,17 @@
+﻿using AuthMetodology.Infrastructure.Interfaces;
+using AuthMetodology.Infrastructure.Models;
+
+namespace MethodologyMain.Infrastructure.Services
+{
+    public class LogQueueService : ILogQueueService
+    {
+        private readonly IRabbitMqService rabbitMqService;
+        private readonly string QueueName = "LogQueue";
+        public LogQueueService(IRabbitMqService rabbitMqService) => this.rabbitMqService = rabbitMqService;
+
+        public async Task SendLogEventAsync(RabbitMqLogPublish message)
+        {
+            await rabbitMqService.SendMessageAsync(message, QueueName);
+        }
+    }
+}
