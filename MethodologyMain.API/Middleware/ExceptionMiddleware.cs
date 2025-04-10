307ea1ff4d3712f5dd25f36935664edc304029e8
BelugaChan <tuchnyy@list.ru>
2025-04-10 19:33:45 +0300

    Added new middleware for handling exceptions
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ MethodologyMain.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using AuthMetodology.Infrastructure.Interfaces;
+using MethodologyMain.Application.Exceptions;
+using Serilog.Events;
+
+namespace MethodologyMain.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogQueueService logQueueService;
+        public ExceptionMiddleware(RequestDelegate next, ILogQueueService logQueueService)
+        {
+            this.next = next;
+            this.logQueueService = logQueueService;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await logQueueService.SendLogEventAsync(new AuthMetodology.Infrastructure.Models.RabbitMqLogPublish {
+                    ServiceName = "Main service",
+                    LogLevel = LogEventLevel.Error,
+                    Message = $"Exception was thrown.\nMessage: {ex.Message}\nSource: {ex.Source}",
+                    TimeStamp = DateTime.UtcNow
+                });
+                await HandleExceptionAsync(ex,context);
+            }
+        }
+
+        private async Task HandleExceptionAsync(Exception ex, HttpContext context)
+        {
+            ExceptionResponse response = ex switch
+            {
+                MemberAlreadyInTeamException _ => new ExceptionResponse("Пользователь уже состоит в команде для данного события", System.Net.HttpStatusCode.Conflict),
+                TeamNotFoundException _ => new ExceptionResponse("Команда не найдена", System.Net.HttpStatusCode.Conflict),
+                UnauthorizedAccessException _ => new ExceptionResponse(ex.Message, System.Net.HttpStatusCode.Unauthorized),
+                InvalidOperationException _ => new ExceptionResponse(ex.Message,System.Net.HttpStatusCode.Conflict),
+                UserNotFoundException _ => new ExceptionResponse("Пользователь не найден",System.Net.HttpStatusCode.Conflict),
+            };
+        }
+    }
+}
