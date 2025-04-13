6d339ed4f2826cc35731e735c4e2429756cd291c
wer3454 <IVAN3-06@mail.ru>
2025-04-13 23:24:06 +0300

    Modify ExceptionMiddleware and add exception

--- MethodologyMain.API/Middleware/ExceptionMiddleware.cs
+++ MethodologyMain.API/Middleware/ExceptionMiddleware.cs
@@ -28,19 +28,21 @@ namespace MethodologyMain.API.Middleware
                     Message = $"Exception was thrown.\nMessage: {ex.Message}\nSource: {ex.Source}",
                     TimeStamp = DateTime.UtcNow
                 });
-                await HandleExceptionAsync(ex,context);
+                HandleException(ex,context);
             }
         }
 
-        private async Task HandleExceptionAsync(Exception ex, HttpContext context)
+        private static void HandleException(Exception ex, HttpContext context)
         {
             ExceptionResponse response = ex switch
             {
                 MemberAlreadyInTeamException _ => new ExceptionResponse("Пользователь уже состоит в команде для данного события", System.Net.HttpStatusCode.Conflict),
                 TeamNotFoundException _ => new ExceptionResponse("Команда не найдена", System.Net.HttpStatusCode.Conflict),
-                UnauthorizedAccessException _ => new ExceptionResponse(ex.Message, System.Net.HttpStatusCode.Unauthorized),
-                InvalidOperationException _ => new ExceptionResponse(ex.Message,System.Net.HttpStatusCode.Conflict),
-                UserNotFoundException _ => new ExceptionResponse("Пользователь не найден",System.Net.HttpStatusCode.Conflict),
+                UnauthorizedAccessException _ => new ExceptionResponse("У вас не таких прав", System.Net.HttpStatusCode.Unauthorized),
+                InvalidOperationException _ => new ExceptionResponse(ex.Message, System.Net.HttpStatusCode.Conflict),
+                UserNotFoundException _ => new ExceptionResponse("Пользователь не найден", System.Net.HttpStatusCode.Conflict),
+                UserNotInTeamException _ => new ExceptionResponse("Пользователь не в команде", System.Net.HttpStatusCode.Conflict),
+                _ => throw new NotImplementedException(),
             };
         }
     }
---
+++ MethodologyMain.Application/Exceptions/UserNotInTeamException.cs
@@ -0,0 +1,16 @@
+﻿namespace MethodologyMain.Application.Exceptions
+{
+    public class UserNotInTeamException : Exception
+    {
+        public UserNotInTeamException()
+        {
+            
+        }
+
+        public UserNotInTeamException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+}
