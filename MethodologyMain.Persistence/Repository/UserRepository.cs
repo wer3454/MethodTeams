87a1d0823cf9abad1c21f73f09107abb98fe1ed5
wer3454 <IVAN3-06@mail.ru>
2025-04-22 02:08:13 +0300

    Add User Repository

---
+++ MethodologyMain.Persistence/Repository/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using MethodologyMain.Logic.Entities;
+using MethodologyMain.Logic.Models;
+using MethodTeams.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodologyMain.Persistence.Repository
+{
+    public class UserRepository: GenericRepository<UserMainEntity>
+    {
+        public UserRepository(MyDbContext context) : base(context)
+        {
+        }
+
+
+    }
+}
