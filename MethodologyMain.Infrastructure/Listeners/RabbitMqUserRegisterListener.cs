6e94400f2d6e4a9ce9ec29a9d985200d32a9e70e
BelugaChan <tuchnyy@list.ru>
2025-04-23 16:14:49 +0300

    Create new register user listener
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ MethodologyMain.Infrastructure/Listeners/RabbitMqUserRegisterListener.cs
@@ -0,0 +1,28 @@
+﻿using MethodologyMain.Infrastructure.Models;
+using MethodologyMain.Logic.Entities;
+using MethodologyMain.Persistence.Interfaces;
+using Microsoft.Extensions.Options;
+using RabbitMqListener.Abstract;
+using RabbitMqModel.Models;
+
+namespace MethodologyMain.Infrastructure.Listeners
+{
+    public class RabbitMqUserRegisterListener : RabbitMqListenerBase<RabbitMqUserRegisterRecieve>
+    {
+        private readonly IUserRepository userRepository;
+        public RabbitMqUserRegisterListener(IUserRepository userRepository, IOptions<RabbitMqOptions> options) : base(options)
+        {
+            this.userRepository = userRepository;
+        }
+
+        protected override string QueueName => "RegisterUserQueue";
+
+        public override async Task ProcessMessageAsync(RabbitMqUserRegisterRecieve message)
+        {
+            await userRepository.AddAsync(new UserMainEntity
+            {
+                Id = message.UserId
+            });
+        }
+    }
+}
