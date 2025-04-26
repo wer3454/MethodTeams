d346949a1541d310055443b9bb44f56f49167886
wer3454 <IVAN3-06@mail.ru>
2025-04-26 19:25:19 +0300

    Add DTO

---
+++ MethodologyMain.Application/DTO/GetTeamDto.cs
@@ -0,0 +1,17 @@
+﻿using MethodologyMain.Logic.Entities;
+
+namespace MethodologyMain.Application.DTO
+{
+    class GetTeamDto
+    {
+        public Guid Id { get; set; }
+        public Guid HackathonId { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = null!;
+        public List<string> Members { get; set; } = null!;
+        public int MaxMembers { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
