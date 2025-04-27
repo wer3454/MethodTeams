e8c4a556365e2f92c35299076cccc8de493f6352
wer3454 <IVAN3-06@mail.ru>
2025-04-27 14:20:31 +0300

    Add cancelationtoken for HackathonRepository

--- MethodologyMain.Persistence/Interfaces/IHackathonRepository.cs
+++ MethodologyMain.Persistence/Interfaces/IHackathonRepository.cs
@@ -5,12 +5,10 @@ namespace MethodologyMain.Persistence.Interfaces
 {
     public interface IHackathonRepository
     {
-        Task<HackathonEntity?> GetByIdAsync(Guid id);
-
-        Task<List<HackathonEntity>> GetAllCurrentHackathonsAsync(int page = 1, int pageSize = 10);
-
-        Task<List<HackathonEntity>> GetAllHackathonsAsync(int page = 1, int pageSize = 10);
-
-        Task<List<HackathonEntity>> GetHackathonsByFlexibleSearchAsync(int page, int pageSize, Expression<Func<HackathonEntity, bool>> filter = null);
+        Task<HackathonEntity?> GetByIdAsync(Guid id, CancellationToken token = default);
+        Task<List<HackathonEntity>> GetAllCurrentHackathonsAsync(int page = 1, int pageSize = 10, CancellationToken token = default);
+        Task<List<HackathonEntity>> GetAllHackathonsAsync(CancellationToken token = default);
+        Task<List<HackathonEntity>> GetAllHackathonsPagedAsync(int page = 1, int pageSize = 10, CancellationToken token = default);
+        Task<List<HackathonEntity>> GetHackathonsByFlexibleSearchAsync(int page, int pageSize, Expression<Func<HackathonEntity, bool>> filter = null, CancellationToken token = default);
     }
 }
