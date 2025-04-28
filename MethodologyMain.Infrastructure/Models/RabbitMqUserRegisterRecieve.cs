0b428064c9e62171f5067001c4452abceaed2580
BelugaChan <tuchnyy@list.ru>
2025-04-28 16:45:42 +0300

    Add new prop username
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

--- MethodologyMain.Infrastructure/Models/RabbitMqUserRegisterRecieve.cs
+++ MethodologyMain.Infrastructure/Models/RabbitMqUserRegisterRecieve.cs
@@ -6,6 +6,8 @@ namespace MethodologyMain.Infrastructure.Models
     {
         public Guid UserId { get; set; }
 
+        public string UserName { get; set; }
+
         public string EventType => "RecieveUserRegister";
     }
 }
