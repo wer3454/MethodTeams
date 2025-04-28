8014a4c6488ee787234331d435daa1a04402b9cb
BelugaChan <tuchnyy@list.ru>
2025-04-28 12:37:29 +0300

    Create listener for rabbitMq (user register)
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

--- MethodologyMain.Infrastructure/Listeners/RabbitMqUserRegisterListener.cs
+++ MethodologyMain.Infrastructure/Listeners/RabbitMqUserRegisterListener.cs
@@ -1,28 +1,48 @@
-﻿using MethodologyMain.Infrastructure.Models;
+﻿using AuthMetodology.Infrastructure.Models;
+using MethodologyMain.Infrastructure.Models;
 using MethodologyMain.Logic.Entities;
 using MethodologyMain.Persistence.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using RabbitMqListener.Abstract;
 using RabbitMqModel.Models;
+using RabbitMqPublisher.Interface;
+using Serilog.Events;
+using System.Text.Json;
 
 namespace MethodologyMain.Infrastructure.Listeners
 {
-    public class RabbitMqUserRegisterListener : RabbitMqListenerBase<RabbitMqUserRegisterRecieve>
+    public class RabbitMqUserRegisterListener : RabbitMqListenerBase
     {
-        private readonly IUserRepository userRepository;
-        public RabbitMqUserRegisterListener(IUserRepository userRepository, IOptions<RabbitMqOptions> options) : base(options)
+        private readonly IServiceProvider serviceProvider;
+        private readonly IRabbitMqPublisherBase<RabbitMqLogPublish> logPublishService;
+        public RabbitMqUserRegisterListener(IRabbitMqPublisherBase<RabbitMqLogPublish> logPublishService, IServiceProvider serviceProvider, IOptions<RabbitMqOptions> options) : base(options)
         {
-            this.userRepository = userRepository;
+            this.logPublishService = logPublishService;
+            this.serviceProvider = serviceProvider;
         }
 
         protected override string QueueName => "RegisterUserQueue";
 
-        public override async Task ProcessMessageAsync(RabbitMqUserRegisterRecieve message)
+        public override async Task ProcessMessageAsync(string message)
         {
-            await userRepository.AddAsync(new UserMainEntity
+            var data = JsonSerializer.Deserialize<RabbitMqUserRegisterRecieve>(message)!;
+            _ = logPublishService.SendEventAsync(new RabbitMqLogPublish
             {
-                Id = message.UserId
+                ServiceName = "Main service",
+                LogLevel = LogEventLevel.Information,
+                Message = "Method ProcessMessageAsync to register a user was called",
+                TimeStamp = DateTime.UtcNow
             });
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<IUserRepository>();
+                await context.AddAsync(new UserMainEntity
+                {
+                    Id = data.UserId
+                });
+            }
+            
         }
     }
 }
