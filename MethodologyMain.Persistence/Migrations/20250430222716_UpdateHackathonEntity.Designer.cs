fef3c81d08dc632fd872781f6e816de8cf5e88e1
wer3454 <IVAN3-06@mail.ru>
2025-05-01 01:39:31 +0300

    Update HackathonEntity with Migrations

--- MethodologyMain.Persistence/Migrations/20250430222716_UpdateHackathonEntity.Designer.cs
+++ MethodologyMain.Persistence/Migrations/20250430222716_UpdateHackathonEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MethodTeams.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MethodologyMain.Persistence.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250430222716_UpdateHackathonEntity")]
+    partial class UpdateHackathonEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
